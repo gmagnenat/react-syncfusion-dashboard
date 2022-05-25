9bc8aa7f6cd9670003f47c0b12decf3ab8117a5a
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-05-25 16:18:32 -0400

    Complete Kanban

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import {
+	KanbanComponent,
+	ColumnsDirective,
+	ColumnDirective,
+} from '@syncfusion/ej2-react-kanban';
+
+import { Header } from '../../components';
 
 const Line = () => {
-	return <div>Line</div>;
+	return (
+		<div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+			<Header category='Charts' title='Line' />
+		</div>
+	);
 };
 
 export default Line;
--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import {
+	KanbanComponent,
+	ColumnsDirective,
+	ColumnDirective,
+} from '@syncfusion/ej2-react-kanban';
+
+import { Header } from '../../components';
 
 const Pyramid = () => {
-	return <div>Pyramid</div>;
+	return (
+		<div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+			<Header category='Charts' title='Pyramid' />
+		</div>
+	);
 };
 
 export default Pyramid;
